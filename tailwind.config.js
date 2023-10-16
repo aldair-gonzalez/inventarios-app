ae843bef307176f37640cae801fc935bd7dc5c1b
Aldair González <aldairgome97@gmail.com>
2023-10-16 17:05:50 -0600

    Actualizando colores de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
+    colors: {
+      white: "#FFFFFF",
+      black: {
+        300: "#2222224d",
+        900: "#222222",
+      },
+      gray: {
+        100: "#EEEEEE",
+        900: "#D1D1D1"
+      }
+    }
   },
   plugins: [],
 }
