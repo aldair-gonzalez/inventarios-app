45b43bad56bc86bb070e0e293b2d703f9654938f
Aldair González <aldairgome97@gmail.com>
2023-10-16 18:07:02 -0600

    Añadiendo el layout a los tickets

---
+++ src/routes/tickets/layout.tsx
@@ -0,0 +1,10 @@
+import { Slot, component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <>
+      Este es el layout de tickets
+      <Slot />
+    </>
+  );
+});
