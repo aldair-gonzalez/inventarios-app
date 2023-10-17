54fbf43c0bcd5e832d545b1797bd374568ce0e95
Aldair González <aldairgome97@gmail.com>
2023-10-16 18:06:48 -0600

    Optimizando el navbar

--- src/routes/inventories/navbar.tsx
+++ src/routes/inventories/navbar.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { Link, useLocation } from '@builder.io/qwik-city';
 import type { IRoutes } from '~/provider/Provier';
 
 export default component$(({ routes, pathname }: { routes: IRoutes[], pathname: string }) => {
@@ -8,19 +8,23 @@ export default component$(({ routes, pathname }: { routes: IRoutes[], pathname: 
     <>
       <div class="flex flex-col gap-2">
         {
-          routes.map((route) => {
-            return (
-              <Link
-                key={route.id}
-                href={`${pathname}${route.href}`}
-                class="w-full py-3 px-4 rounded-md uppercase text-sm hover:bg-gray-100 dark:hover:text-black-900"
-                >
-                {route.title}
-              </Link>
-            );
-          })
+          routes.map((route) => <ButtonNavigation route={route} pathname={pathname} key={route.id}/>)
         }
       </div>
+    </>
+  );
+});
+
+const ButtonNavigation = component$(({ route, pathname }: { route: IRoutes, pathname: string }) => {
+  const path = useLocation().url.pathname;
+  return (
+    <>
+      <Link
+        href={`${pathname}${route.href}`}
+        class={`w-full py-3 px-4 rounded-md uppercase text-sm hover:bg-gray hover:text-white ${path.includes(route.href) ? 'bg-gray text-white' : ''}`}
+        >
+        {route.title}
+      </Link>
     </>
   );
 });
