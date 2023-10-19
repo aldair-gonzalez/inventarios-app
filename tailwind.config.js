5484ed6f5f58a8a03330b39ecb0705d07fa937bb
Aldair González <aldairgome97@gmail.com>
2023-10-18 20:47:36 -0600

    Añadiento nuevas configuraciones a tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ export default {
   theme: {
     extend: {},
     colors: {
-      white: '#FAF7F2',
+      white: {
+        DEFAULT: '#FAF7F2',
+        300: '#FAF7F24d',
+        500: '#FAF7F280',
+      },
       black: {
+        DEFAULT: '#2B2B2A',
         300: '#2B2B2A4d',
-        900: '#2B2B2A',
       },
       gray: '#5C5A59',
       transparent: 'transparent'
